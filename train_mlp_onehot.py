83ac64914eda3c3d89a0a0cfa3f1c9d68321247d
dohyeoklee <dohyeoklee.kr@gmail.com>
2022-05-31 12:19:13 +0900

    2022.05.31.home.1

--- train_mlp_onehot.py
+++ train_mlp_onehot.py
@@ -24,15 +24,8 @@ class SubwayDataset(Dataset):
 			self.test_sampler = SubsetRandomSampler(test_idxs)
 
 			if scaling:
-				_x_train = np.array([x_data[i] for i in train_idxs])
-				_x_data = np.array(x_data)
-				x_min = np.min(_x_train,axis=0)
-				x_max = np.max(_x_train,axis=0)
-				x_range = np.reciprocal(x_max - x_min,dtype=float)
-				x_min_arr = np.repeat(np.array([x_min]),repeats=_x_data.shape[0],axis=0)
-				x_range_arr = np.repeat(np.array([x_range]),repeats=_x_data.shape[0],axis=0)
-				x_scaled = np.multiply((_x_data - x_min_arr),x_range_arr)
-				self.train_x_data = x_scaled.tolist()
+				#self.train_x_data = self.min_max_scaler(x_data,train_idxs)
+				self.train_x_data = self.robust_scaler(x_data,train_idxs)
 			else:
 				self.train_x_data = x_data
 		else:
@@ -46,12 +39,40 @@ class SubwayDataset(Dataset):
 		y = torch.FloatTensor(self.train_y_data[idx])
 		return x,y
 
+	def min_max_scaler(self,x_data,train_idxs):
+		_x_train = np.array([x_data[i] for i in train_idxs])
+		_x_data = np.array(x_data)
+		x_min = np.min(_x_train,axis=0)
+		x_max = np.max(_x_train,axis=0)
+		x_range = np.reciprocal(x_max - x_min,dtype=float)
+		x_min_arr = np.repeat(np.array([x_min]),repeats=_x_data.shape[0],axis=0)
+		x_range_arr = np.repeat(np.array([x_range]),repeats=_x_data.shape[0],axis=0)
+		x_scaled = np.multiply((_x_data - x_min_arr),x_range_arr)
+		return x_scaled.tolist()
+
+	def robust_scaler(self,x_data,train_idxs):
+		_x_train = np.array([x_data[i] for i in train_idxs])
+		_x_data = np.array(x_data)
+		percentile = np.nanpercentile(_x_data,[25,50,75],axis=0)
+		x_1q = percentile[0]
+		x_med = percentile[1]
+		x_3q = percentile[2]
+		x_iqr = np.reciprocal(x_3q - x_1q,dtype=float)
+		x_med_arr = np.repeat(np.array([x_med]),repeats=_x_data.shape[0],axis=0)
+		x_iqr_arr = np.repeat(np.array([x_iqr]),repeats=_x_data.shape[0],axis=0)
+		x_scaled = np.multiply((_x_data - x_med_arr),x_iqr_arr)
+		#print(x_scaled)
+		#x_scaled = np.clip(x_scaled,-1.0,1.0)
+		print(x_scaled.shape)
+		return x_scaled.tolist()
+
 	def huristic_processing(self,path):
 		df = pd.read_csv(path)
 		df.drop(['Next Train ID'],axis=1,inplace=True)
 		df.drop(['Final Train ID'],axis=1,inplace=True)
 		result = df.groupby(['BS_1_1','RSRP_1_1','RSSI_1_1','RSRQ_1_1',\
 			'BS_2_1','RSRP_2_1','RSSI_2_1','RSRQ_2_1']).aggregate([np.mean])
+		result = result.fillna(0.0)['Distance from station']
 		result = self.huristic_processing_equal_space(result)
 		input_list = result.index.tolist()
 		input_list = list(map(list,input_list))
@@ -59,7 +80,8 @@ class SubwayDataset(Dataset):
 		return input_list,label_list
 
 	def huristic_processing_equal_space(self,df):
-		return df.apply(lambda x: 10*int(x/10)+5,axis=1)
+		space = 10
+		return df.apply(lambda x: space*int(x/space)+space/2,axis=1)
 
 	def get_sampler(self):
 		return self.train_sampler,self.test_sampler
@@ -103,9 +125,16 @@ def loss_fn(pred,target):
 def train(seed,target_scenario,result_data):
 	data_dir = os.path.join("./data/processed_data/split",target_scenario)
 
-	batch_size = 32
+	#default setting
+	#batch_size = 32
+	#hidden_size = 12
+	#lr = 5e-2
+	#weight_decay=1e-3
+
+	batch_size = 64
 	hidden_size = 12
 	lr = 5e-2
+	weight_decay=1e-3
 	test = True
 	scaling = True
 
@@ -127,7 +156,7 @@ def train(seed,target_scenario,result_data):
 	model = Mlp(hidden_size)
 	model = model.to(device)
 	model.apply(init_weights)
-	optimizer = torch.optim.Adam(model.parameters(),lr=lr,weight_decay=1e-3) #weight_decay=1e-3
+	optimizer = torch.optim.Adam(model.parameters(),lr=lr,weight_decay=weight_decay)
 	scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=500,gamma=0.8)
 
 	num_epoch = 3000
@@ -153,8 +182,8 @@ def train(seed,target_scenario,result_data):
 		if test:
 			with torch.no_grad():
 				mean_error,worst_5_error,worst_error = test_model(model,test_dataloader,device)
-				#print('Epoch {:4d}/{}, mean error: {:.6f}, worst error: {:.6f}'\
-				#	.format(epoch,num_epoch,mean_error,max_error))
+				print('Epoch {:4d}/{}, mean error: {:.6f}, worst error: {:.6f}'\
+					.format(epoch,num_epoch,mean_error,worst_error))
 				mean_error_list.append(mean_error)
 				worst_5_error_list.append(worst_5_error)
 				worst_error_list.append(worst_error)
@@ -172,10 +201,12 @@ if __name__ == '__main__':
 	result_dir = "./result/mlp_onehot/"
 	result_data_list = []
 
-	seeds = [1991,202205,20220502]
+	#seeds = [1991,202205,20220502]
+	seeds = [2022,199152,5020348]
 	root_dir = "./data"
 	data_root_dir = os.path.join(root_dir,"processed_data/split")
-	data_list = os.listdir(data_root_dir)
+	#data_list = os.listdir(data_root_dir)
+	data_list = ["re_13.csv"]
 	for data_name in tqdm(data_list,desc="data loop"):
 		for seed in seeds:
 			result_data_list = train(seed,data_name,result_data_list)
